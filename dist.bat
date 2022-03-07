c33b58237d1e91f2ad08c5dc96914c7fb581e93a
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-03-07 21:16:25 +0100

    Dist script improved

--- dist.bat
+++ dist.bat
@@ -3,6 +3,8 @@ rmdir /S /Q dist
 mkdir dist
 cd client
 tar -cvf ..\dist\client.tar.gz cli.py lns.py
+pyinstaller cli.py --onefile
+copy .\dist\cli.exe ..\dist\cli.exe
 cd ..\server
 tar -cvf ..\dist\server.tar.gz *
 cd ..
