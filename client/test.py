9648714f5dd987e4a97df87fa5d15d3d6df2a0ba
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-03-08 18:00:37 +0100

    tests improved

--- client/test.py
+++ client/test.py
@@ -1,31 +1,37 @@
 import unittest
 from lns import Client, IncompatibleServer, NameNotFound
 
+server_url = 'omicronlns.glitch.me'
+
 class LNSTest(unittest.TestCase):
     def test_lns_server_http(self):
         #Test connection to server with http scheme
-        Client('https://omicronlns.glitch.me')
+        Client('https://' + server_url)
 
     def test_lns_server_lns(self):
         #Test connection to server with lns scheme
-        Client('lns://omicronlns.glitch.me')
+        Client('lns://' + server_url)
 
     def test_lns_server_schemeless(self):
         #Test connection to server without scheme
-        Client('omicronlns.glitch.me')
+        Client(server_url)
 
+    def test_not_server_detection(self):
         #test exception raise
         with self.assertRaises(IncompatibleServer):
             Client('https://google.com')
         
     def test_lns_resolver(self):
         #test the resolver system
-        lns = Client('https://omicronlns.glitch.me')
+        lns = Client('https://' + server_url)
         self.assertEqual(lns.resolve('template'), 'dns.google.com')
+
+        with self.assertRaises(NameNotFound):
+            lns.resolve('paraguay')
 
     def test_lns_dig(self):
         #test the dig system
-        lns = Client('https://omicronlns.glitch.me')
+        lns = Client('https://' + server_url)
         self.assertEqual(lns.dig('template'), {
             "record_owner": {
                 "name": "Omicron166",
@@ -36,6 +42,9 @@ class LNSTest(unittest.TestCase):
                 "txt": "dns like txt record"
             }
         })
+
+        with self.assertRaises(NameNotFound):
+            lns.dig('paraguay')
 
 if __name__ == '__main__':
     unittest.main()
