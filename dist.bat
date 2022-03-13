a601f4c5e9e2d5f1507930cf76371e797f5513e9
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-03-13 11:24:05 +0100

    Update dist.bat

--- dist.bat
+++ dist.bat
@@ -1,11 +1,21 @@
 @echo off
+
+rem #dist clean
 rmdir /S /Q dist
 mkdir dist
+
+rem #client package
 cd client
 tar -cvf ..\dist\client.tar.gz cli.py lns.py
 pyinstaller cli.py --onefile
 copy .\dist\cli.exe ..\dist\cli.exe
-cd ..\server
-tar -cvf ..\dist\server.tar.gz *
-cd ..
+
+rem #server-fs package
+cd ..\server-fs
+tar -cvf ..\dist\server-fs.tar.gz *
+
+rem #server-bin package
+cd ..\server-bin
+pyinstaller server.py --onefile
+copy .\dist\server.exe ..\dist\server.exe
 @echo on
